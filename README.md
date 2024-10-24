db3fda695d9496fc03bfc1c56fb2caaae6e824ac
Maddy Guthridge <hello@maddyguthridge.com>
2024-10-24 17:48:28 +1100

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# comp4920-summary
-My summary of the content of COMP4920 in 24T3
+# COMP4920 Summary
+
+My summary of the content of COMP4920 in 24T3.
+
+## Generating the website
+
+```sh
+poetry run python generate.py > index.html
+```
---
+++ generate.py
@@ -0,0 +1,66 @@
+"""
+generate.py
+
+Author: Maddy Guthridge
+"""
+from typing import Callable
+import pyhtml as p
+import random
+
+terms = [
+    *(["AI"] * 10),
+    *(["ai"] * 4),
+    *(["artificial intelligence"] * 5),
+    *(["Artificial Intelligence"] * 3),
+    *(["machine learning"] * 7),
+    *(["ML"] * 4),
+    *(["large language models"] * 6),
+    *(["LLMs"] * 4),
+    *(["neural networks"] * 3),
+    "ADMS",
+    *(["automated decision-making systems"] * 3),
+]
+
+elements: list[Callable[[str], p.Tag]] = [
+    *([p.span] * 20),
+    *([p.i] * 10),
+    *([p.b] * 5),
+    *([p.u] * 3),
+    p.em,
+    p.small,
+    lambda s: p.b(p.i(s)),
+    lambda s: p.b(p.i(s)),
+    lambda s: p.b(p.u(s)),
+    lambda s: p.u(p.i(s)),
+    lambda s: p.u(p.i(p.b(s))),
+]
+
+paragraphs = []
+
+for _ in range(500):
+    if random.random() > 0.7:
+        paragraphs.append(random.choice([p.ol, p.ul])([
+            p.li(random.choice(elements)(random.choice(terms)))
+            for _ in range(random.randint(5, 50))
+        ]))
+    else:
+        paragraphs.append(p.p([
+            random.choice(elements)(f"{random.choice(terms)},")
+            for _ in range(random.randint(10, 100))
+        ]))
+
+
+print(p.html(
+    p.head(p.title("COMP4920 Summary")),
+    p.body(
+        p.h1("COMP4920 Summary"),
+        p.p("""
+            This page contains a summary of the course content, including
+            lecture, tutorial and assignment content for UNSW's COMP4920
+            "Professional Issues and Ethics in Information Technology" course.
+        """),
+        p.p("I hope you find it helpful!"),
+        paragraphs,
+        p.p("Thank you for reading. I hope your experience was enjoyable!"),
+    ),
+))
